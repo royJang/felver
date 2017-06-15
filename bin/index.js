9247c5f8cf27f677924751689fed50ff257f10df
royjang <zzl11081@gmai.com>
2017-06-15 10:08:21 +0800

    Update 2017-06-15T02:08:03.989Z

--- bin/index.js
+++
@@ -1,9 +0,0 @@
-import * as geometry from './geometry';
-import * as util from './util';
-import * as buffer from './buffer';
-
-module.exports = {
-    util,
-    buffer,
-    geometry
-};
--- build/loader-generator.js
+++
@@ -1,7 +0,0 @@
-['STL', 'OBJ', 'JSON', 'OBJ-MTL', 'FBX'].forEach( format => {
-    var result = [];
-    result.push(`import \$${format}Loader from '../assets/${format}Loader'; 
-        export const STLLoader = \$${format}Loader;`);
-    result.join('\n');
-    fs.outputFileSync('');
-});
